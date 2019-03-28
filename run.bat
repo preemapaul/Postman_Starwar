9dbf686a98f4a407b40af7e98867f6ba22415346
preemapaul <48873102+preemapaul@users.noreply.github.com>
2019-03-28 01:19:37 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-C:\Users\Joju\AppData\Roaming\npm\newman run Starwar Assignment.json -d Data2.csv --reporters cli,html --reporter-cli --reporter-html-export  Postman_Starwar\export.html --disable-unicode
+C:\Users\Joju\AppData\Roaming\npm\newman run StarwarAssignment.json -d Data2.csv --reporters cli,html --reporter-cli --reporter-html-export  Postman_Starwar\export.html --disable-unicode
