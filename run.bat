2bfad5cda7b4608362442ffa7fb6056a1b796ed4
preemapaul <48873102+preemapaul@users.noreply.github.com>
2019-03-28 02:49:01 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-C:\Users\Joju\AppData\Roaming\npm\newman run StarwarAssignment.json -d Data2.csv --reporters cli,htmlextra --reporter-cli --reporter-htmlextra-export C:\Users\Joju\Desktop\Starwars\export3.html --disable-unicode
+C:\Users\Joju\AppData\Roaming\npm\newman run StarwarAssignment.json -d Data2.csv --reporters cli,htmlextra --reporter-cli --reporter-htmlextra-export export.html --disable-unicode
